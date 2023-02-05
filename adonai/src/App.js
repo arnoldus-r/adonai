a2b652c184ee909e508006238066e1345699c007
arnoldus-r <122922707+arnoldus-r@users.noreply.github.com>
2023-02-04 22:15:16 -0400

    Remove unused file

--- adonai/src/App.js
+++ adonai/src/App.js
@@ -1,6 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-
 import { ThemeProvider } from "@mui/material/styles";
 import theme from './components/Theme';
 import Login from './pages/Login';
