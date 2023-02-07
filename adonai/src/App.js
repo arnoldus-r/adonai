05742fd82341829d8054b48a6ef5757ea38839f4
jesymarlop <86635217+jesymarlop@users.noreply.github.com>
2023-02-06 21:21:21 -0400

    Queda en blanco

--- adonai/src/App.js
+++ adonai/src/App.js
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import theme from './components/Theme';
 import Login from './pages/Login';
 import UserTable from './components/UserTable';
+import Menu from './components/Menu';
 
 import { CssBaseline } from '@mui/material';
 
@@ -9,8 +10,10 @@ function App() {
 	return (
 		<ThemeProvider theme={ theme }>
 			<CssBaseline>
+				<Menu />
 				<Login />
 				<UserTable />
+				
 			</CssBaseline>
 		</ThemeProvider>
 	);
