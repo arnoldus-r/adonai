f71723842a3f01408df700b28fabe8af7f2b998c
arnoldus-r <122922707+arnoldus-r@users.noreply.github.com>
2023-02-06 21:50:26 -0400

    Fixes warnings

--- adonai/src/App.js
+++ adonai/src/App.js
@@ -1,32 +1,28 @@
-import * as React from 'react';
-import { BrowserRouter, Routes, Navigate, Switch, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from '@mui/material';
+
 import theme from './components/Theme';
 import Login from './pages/Login';
 import UserTable from './components/UserTable';
 import Report from './components/Report';
 
-
-
-import { CssBaseline } from '@mui/material';
-
 function App() {
-	const token = localStorage.getItem('@token') || null;
 
 	return (
-		<BrowserRouter>
-		<ThemeProvider theme={ theme }>
-			<CssBaseline>
-			<Routes> 
-			  <Route exact path='/' element={<Login />} /> 
-			  	 <Route path='/user' element={<UserTable />} />
-			  	 <Route path='/report' element={<Report />} /> 
-
-			</Routes>
-			</CssBaseline>
-		</ThemeProvider>
-		</BrowserRouter>
+		<Router>
+			<ThemeProvider theme={ theme }>
+				<CssBaseline>
+					<Routes> 
+						<Route exact path='/' element={<Login />} /> 
+						<Route path='/user' element={<UserTable />} />
+						<Route path='/report' element={<Report />} /> 
+					</Routes>
+				</CssBaseline>
+			</ThemeProvider>
+		</Router>
 	);
 }
 
--- adonai/src/pages/Login.js
+++ adonai/src/pages/Login.js
@@ -8,76 +8,74 @@ import { useNavigate } from "react-router-dom";
 
 
 function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      {'Adonai Jireh '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+	return (
+		<Typography variant="body2" color="text.secondary" align="center" {...props}>
+			{'Copyright © '}
+			{'Adonai Jireh '}
+			{new Date().getFullYear()}
+			{'.'}
+		</Typography>
+	);
 }
 
-
-
 export default function Login() {
-      const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-    localStorage.setItem('@token', data.get('email'));
-    if (data.get('email') === '0') {  navigate("/user")}
-      else if (data.get('email') === '1') { navigate("report")}
-  };
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+		
+		if (data.get('email') === '0') {
+			navigate("/user")
+		}
+		else if (data.get('email') === '1') {
+			navigate("/report")
+		}
+	};
 
-  return (
-      <Container component="main" maxWidth="xs">
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Typography component="h1" variant="h5">
-            Iniciar Sesión
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              fullWidth
-              id="email"
-              label="Correo"
-              name="email"
-              autoComplete="off"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              fullWidth
-              name="password"
-              label="Contraseña"
-              type="password"
-              id="password"
-              autoComplete="off"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Entrar
-            </Button>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-  );
+	return (
+			<Container component="main" maxWidth="xs">
+				<Box
+					sx={{
+						marginTop: 8,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<Typography component="h1" variant="h5">
+						Iniciar Sesión
+					</Typography>
+					<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+						<TextField
+							margin="normal"
+							fullWidth
+							id="email"
+							label="Correo"
+							name="email"
+							autoComplete="off"
+							autoFocus
+						/>
+						<TextField
+							margin="normal"
+							fullWidth
+							name="password"
+							label="Contraseña"
+							type="password"
+							id="password"
+							autoComplete="off"
+						/>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							sx={{ mt: 3, mb: 2 }}
+						>
+							Entrar
+						</Button>
+					</Box>
+				</Box>
+				<Copyright sx={{ mt: 8, mb: 4 }} />
+			</Container>
+	);
 }
