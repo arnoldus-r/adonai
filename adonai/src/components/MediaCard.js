4501530312988247de6e978f50c991cd78119e31
arnoldus-r <122922707+arnoldus-r@users.noreply.github.com>
2023-02-07 21:29:44 -0400

    4

--- adonai/src/components/MediaCard.js
+++ adonai/src/components/MediaCard.js
@@ -11,7 +11,7 @@ export default function MediaCard() {
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         sx={{ height: 140 }}
-        image="/shop/Freno.jpg"
+        image="./shop/Freno.jpg"
         title="green iguana"
       />
       <CardContent>
